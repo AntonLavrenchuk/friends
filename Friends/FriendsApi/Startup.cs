98572dd5f481119e1f8df676a26c4df7db3a5466
GudzDenis <dengudz04@gmail.com>
2022-06-20 19:30:33 +0300

    Authentication Controlller
    Basic not working logic for Login, Register methods

--- Friends/FriendsApi/Startup.cs
+++ Friends/FriendsApi/Startup.cs
@@ -67,6 +67,7 @@ namespace FriendsApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FriendsApi", Version = "v1" });
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Friends/FriendsAuth/Controllers/AuthenticationController.cs
@@ -0,0 +1,58 @@
+﻿using FriendsAuth.Models;
+using FriendsData;
+using FriendsData.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendsAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private UnitOfWork _unitOfWork;
+
+        public AuthenticationController( UnitOfWork unitOfWork, Mapper )
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Login( LoginModel model )
+        {
+            var exists = _unitOfWork.UsersRepository.Get().Any(usr => usr.Email.Equals(model.Email));
+
+            if(!exists)
+            {
+                return Unauthorized();
+            }
+
+            return Ok();
+        }
+
+        [HttpPost] IActionResult Register(RegisterModel model)
+        {
+
+            var existsWithEmail = _unitOfWork.UsersRepository.Get().Any(usr => usr.Email.Equals(model.Email));
+
+            if(existsWithEmail)
+            {
+                return Conflict();
+            }
+
+            var existsWithPhone = _unitOfWork.UsersRepository.Get().Any(usr => usr.PhoneNumber.Equals(model.PhoneNumber));
+
+            if (existsWithPhone)
+            {
+                return Conflict();
+            }
+            var newUser = new User();
+            
+            this._unitOfWork.UsersRepository.AddAsync(newUser);
+
+            return Ok();
+        }
+
+
+    }
+}
